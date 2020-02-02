8cf2e50d73d0691ed217ce4a577f9131cdea8d10
dusy <dusynity@gmail.com>
2020-02-02 16:22:32 +0800

    文字

--- VDefend/Assets/GameManager.cs
+++ VDefend/Assets/GameManager.cs
@@ -166,10 +166,10 @@ public class GameManager : SimpleSingletonMono<GameManager>,TReflection.UI.IUIPr
 
 
         float gameProgress = 1 - m_DisableCount / (float)m_CellTilePool.m_ActiveItemDic.Count;
-        m_AntibodyTime.text = string.Format("Antibody:{0:F2}", m_TimerAntibody.m_timeCheck);
-        m_GameProgress.text = string.Format("Progress:{0:F2}, Infect:{1}, Virus:{2}", gameProgress, m_InfectCount, m_VirusCount);
+        m_AntibodyTime.text = string.Format("Antibody Coming:{0:F2}", m_TimerAntibody.m_timeCheck);
+        m_GameProgress.text = string.Format("Body Health:{0:F2}, Infected Cells:{1}, Virus:{2}", gameProgress, m_InfectCount, m_VirusCount);
         m_ProgressBar.fillAmount = gameProgress;
-        m_GameTime.text = string.Format("Time:{0:F2}", m_GameTimePassed);
+        m_GameTime.text = string.Format("Game Time:{0:F2}", m_GameTimePassed);
 
         if (gameProgress < GameConsts.F_GameLoseScale)
             OnGameFinish(false);
